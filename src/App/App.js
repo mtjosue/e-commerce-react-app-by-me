67c8477887dd3a36e458082372f59bb9ead46bfd
mtjosue <65678981+mtjosue@users.noreply.github.com>
2021-03-24 18:27:48 -0400

    Beggining components and some SCSS intro

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from '../components/home/HomePage';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>E-Commerce-React-App</h1>
+      <HomePage />
     </div>
   );
 }
